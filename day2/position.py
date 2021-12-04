86a45575e86dcfebdc824335e1872c1f7f310d5f
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-04 09:06:25 -0800

    Add comments

--- day2/position.py
+++ day2/position.py
@@ -1,3 +1,14 @@
+#
+# Goal: Calculate the horizontal position and depth you would have after
+# following the planned course. What do you get if you multiply your
+# final horizontal position by your final depth?
+#
+# Commands:
+#    forward X increases the horizontal position by X units.
+#    down X increases the depth by X units.
+#    up X decreases the depth by X units.
+#
+
 def getPosition():
     f = open("input.txt", "r")
 
