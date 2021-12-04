11a3a075ede39bf52f39a098e8e78bf8d080458f
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-03 18:05:51 -0800

    Fix compariosn bug.

--- day1/depthCount.py
+++ day1/depthCount.py
@@ -3,15 +3,14 @@ def getIncreases():
 
     depths = f.readlines() # create list of depths
     increases = 0
-    previous = depths[0] # the first depth begins as the previous
-    i = 1
     # the total number of increases is computed by comparing the
     # previous distance to the current distance
+    i = 1
     while i < len(depths):
+        previous = depths[i - 1]
         current = depths[i]
-        if (int(current) - int(previous)) < 0:
+        if (int(previous) - int(current)) < 0:
             increases += 1
-        previous = current
         i += 1
 
     return increases
