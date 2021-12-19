fee8584acb0ac7baedf81bb685e3be5ac34e2af5
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-18 18:32:31 -0800

    :bug: in parsing

--- day6/day06.2.py
+++ day6/day06.2.py
@@ -1,11 +1,10 @@
 def parseInput():
-    f = open('testInput.txt', 'r')
-    counts = [0]*8
-    for i in f.readlines():
-        counts[i] += 1
-    # days = 256
-    # fish = [[int(i), days] for i in f.read().split(',')]
-    return counts
+    f = open('input.txt', 'r')
+    fish = [int(i) for i in f.read().split(',')]
+    counts = [0]*9
+    for f in fish:
+        counts[f] += 1
+    return fish, counts
 
 def getTotalChildren(fish):
     currentDay = fish[0]
@@ -32,6 +31,18 @@ def getCounts(fish):
         print(fish)
     return total
 
+def arrayCounts(counts, days):
+    while days > 0:
+        newFish = counts[0]
+        for i in range(len(counts) - 1):
+            counts[i] = counts[i+1]
+        counts[6] += newFish
+        counts[-1] = newFish
+        days -= 1
+    total = sum(counts)
+    return total
+
+
 if __name__ == "__main__":
-    fish = parseInput()
-    print(getCounts(fish ))
+    fish, counts = parseInput()
+    print(getCounts(counts))
