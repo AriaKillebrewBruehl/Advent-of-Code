bd9a9c115b90572ec30506e92de0915d69b6c2a7
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-18 18:17:41 -0800

    Add array implementation for counting

--- day6/day06.1.py
+++ day6/day06.1.py
@@ -1,7 +1,10 @@
 def parseInput():
     f = open('input.txt', 'r')
     fish = [int(i) for i in f.read().split(',')]
-    return fish
+    counts = [0]*9
+    for f in fish:
+        counts[f] += 1
+    return fish, counts
 
 def getCounts(fish, days):
     while days > 0:
@@ -11,10 +14,23 @@ def getCounts(fish, days):
                 fish.append(8)
             else:
                 fish[i] -= 1
-
         days -= 1
     return len(fish)
 
+
+def arrayCounts(counts, days):
+    while days > 0:
+        newFish = counts[0]
+        for i in range(len(counts) - 1):
+            counts[i] = counts[i+1]
+        counts[6] += newFish
+        counts[-1] = newFish
+        days -= 1
+    total = sum(counts)
+    return total
+
+
 if __name__ == "__main__":
-    fish = parseInput()
+    fish, counts = parseInput()
     print(getCounts(fish, 80))
+    print(arrayCounts(counts, 80))
