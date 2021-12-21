fe008eb9b472dbd2fdce5793543dc4b40680ea3c
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-20 21:56:14 -0800

    Remove grid

--- day13/day13.1.py
+++ day13/day13.1.py
@@ -1,9 +1,7 @@
 def parseInput():
-    f = open('testInput.txt', 'r')
+    f = open('input.txt', 'r')
     instructions = []
     dots = []
-    maxX = 0
-    maxY = 0
     for l in f.readlines():
         if 'fold' in l:
             fold = l.strip().split(' ')
@@ -13,16 +11,8 @@ def parseInput():
         else:
             x, y = l.strip().split(',')
             x, y = int(x), int(y)
-            if x > maxX:    maxX = x
-            if y > maxY:    maxY = y
             dots += [[x, y]]
-    grid = [['.' for i in range(maxX + 1)] for j in range(maxY + 1)]
-    return dots, instructions, grid
-
-def markPaper(dots, grid):
-    for d in dots:
-        x, y = d[0], d[1]
-        grid[y][x] = '#'
+    return dots, instructions
 
 def singleFold(dots, instructions):
     fold = instructions[0]
@@ -40,19 +30,7 @@ def singleFold(dots, instructions):
                     dots.append(d)
     return
 
-def countMarks(grid):
-    total = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == '#':
-                total += 1
-    return total
-
 if __name__ == "__main__":
-    dots, instructions, grid = parseInput()
-    markPaper(dots, grid)
-    for g in grid:
-        print(g)
-    print()
+    dots, instructions = parseInput()
     singleFold(dots, instructions)
     print(len(dots))
