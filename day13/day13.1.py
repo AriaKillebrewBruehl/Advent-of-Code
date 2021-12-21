abb4ec50921d785ee99ee7731fc8dc6dd5385817
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-20 20:55:00 -0800

    Implement folding and counting

--- day13/day13.1.py
+++ day13/day13.1.py
@@ -17,7 +17,33 @@ def parseInput():
     grid = [['.' for x in maxX] for y in maxY]
     return dots, instructions, grid
 
-def markPaper(dots):
+def markPaper(dots, grid):
+    for d in dots:
+        x, y = d[0], d[1]
+        grid[y][x] = '#'
+
+def singleFold(grid, instructions):
+    fold = instructions[0]
+    if fold[0] == 'x':
+        line = fold[1]
+    else:
+        line = fold[1]
+        newGrid = [grid[i] for i in range(line)]
+        for i in range(line + 1, len(grid)):
+            for j in grid[i]:
+                newGrid[line - 1 - i][j] = grid[i][j]
+    return newGrid
+
+def countMarks(grid):
+    total = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == '#':
+                total += 1
+    return total
 
 if __name__ == "__main__":
     dots, instructions, grid = parseInput()
+    markPaper(grid)
+    folded = singleFold(grid)
+    print(countMarks(folded))
