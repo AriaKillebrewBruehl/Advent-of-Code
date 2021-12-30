619db0b4e3f354c5202430d3356e64ebc686adf2
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-30 09:32:09 -0800

    Copy getNeighbors from day 11

--- day12/day12.1.py
+++ day12/day12.1.py
@@ -1,7 +1,19 @@
 def parseInput():
-    f = open('tinyInput.txt', 'r')
+    f = open('testInput.txt', 'r')
     connections = {}
     for line in f.readlines():
-        enter, exit = f.split('-')
+        enter, exit = line.strip().split('-')
         connections[enter] = exit
     return connections
+
+def findPaths(connections):
+    numPaths = 0
+    for option in connections['start']:
+        numPaths += 1
+        current = option
+
+
+
+if __name__ == '__main__':
+    connections = parseInput()
+    print(connections)
