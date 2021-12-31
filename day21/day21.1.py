8a49502012fa1cef542521910464ffbe4d7b5eef
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-30 16:54:27 -0800

    Implement die

--- day21/day21.1.py
+++ day21/day21.1.py
@@ -1,9 +1,18 @@
+from typing import Counter
+
+
 def parseInput():
     f = open('input.txt', 'r')
     lines = f.readlines()
     pos1 = lines[0].strip().split(' ')[-1]
     pos2 = lines[1].strip().split(' ')[-1]
     return pos1, pos2
+
+count = 0
+def deterministicDie():
+    global count
+    count += 1
+    return count % 100
 
 if __name__ == '__main__':
     pos1, pos2 = parseInput()
