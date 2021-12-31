ec65bb67adb6dd6b2eeb0ec74b6ae565e213b6d1
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-30 17:41:42 -0800

    Copy day 21 pt 1

---
+++ day21/day21.2.py
@@ -0,0 +1,47 @@
+def parseInput():
+    f = open('input.txt', 'r')
+    lines = f.readlines()
+    pos0 = lines[0].strip().split(' ')[-1]
+    pos1 = lines[1].strip().split(' ')[-1]
+    player0 = [int(pos0), 0]
+    player1 = [int(pos1), 0]
+    return player0, player1
+
+roll = 0
+def deterministicDie():
+    global roll
+    roll += 1
+    return roll % 100
+
+def rollThree():
+    r1, r2, r3 = deterministicDie(), deterministicDie(), deterministicDie()
+    return r1 + r2 + r3
+
+count = 0
+def playGame(player0, player1, turn):
+    global count
+    if player0[1] >= 1000 or player1[1] >= 1000:
+        return 0
+    else:
+        move = rollThree()
+        if not turn:
+            player0[0] = (player0[0] + move) % 10
+            if player0[0] == 0: player0[0] = 10
+            player0[1] += player0[0]
+        else:
+            player1[0] = (player1[0] + move) % 10
+            if player1[0] == 0: player1[0] = 10
+            player1[1] += player1[0]
+        count += 1
+        return playGame(player0, player1, not turn) + 1
+
+def loosingScore(player0, player1):
+    count = playGame(player0, player1, 0) * 3
+    if player0[1] >= 1000:  return count * player1[1]
+    else:                   return count * player0[1]
+
+if __name__ == '__main__':
+    player0, player1 = parseInput()
+    print(loosingScore(player0, player1))
+
+
