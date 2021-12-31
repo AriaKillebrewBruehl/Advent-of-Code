b02f318f5b9633cc89f6e962e4349d4c06be006a
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-30 16:58:51 -0800

    Implement helper to roll three die

--- day21/day21.1.py
+++ day21/day21.1.py
@@ -6,14 +6,24 @@ def parseInput():
     lines = f.readlines()
     pos1 = lines[0].strip().split(' ')[-1]
     pos2 = lines[1].strip().split(' ')[-1]
-    return pos1, pos2
+    player1 = [pos1, 0]
+    player2 = [pos2, 0]
+    return player1, player2
 
-count = 0
+roll = 0
 def deterministicDie():
-    global count
-    count += 1
-    return count % 100
+    global roll
+    roll += 1
+    return roll % 100
 
+def rollThree():
+    r1, r2, r3 = deterministicDie(), deterministicDie(), deterministicDie()
+    return r1 + r2 + r3
+
+def playGame(player1, player2):
+    if player1[1] >= 1000 or player2[1] >= 1000:
+        return 0
+    else:
 if __name__ == '__main__':
-    pos1, pos2 = parseInput()
+    player1, player2 = parseInput()
     print(pos1, pos2)
