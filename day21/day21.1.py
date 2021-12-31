273d796e17a511406ae6e4cc760497cd2085bd7f
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-12-30 17:02:31 -0800

    Implement game play algorithm

--- day21/day21.1.py
+++ day21/day21.1.py
@@ -4,11 +4,11 @@ from typing import Counter
 def parseInput():
     f = open('input.txt', 'r')
     lines = f.readlines()
-    pos1 = lines[0].strip().split(' ')[-1]
-    pos2 = lines[1].strip().split(' ')[-1]
+    pos0 = lines[0].strip().split(' ')[-1]
+    pos1 = lines[1].strip().split(' ')[-1]
+    player0 = [pos0, 0]
     player1 = [pos1, 0]
-    player2 = [pos2, 0]
-    return player1, player2
+    return player0, player1
 
 roll = 0
 def deterministicDie():
@@ -20,10 +20,18 @@ def rollThree():
     r1, r2, r3 = deterministicDie(), deterministicDie(), deterministicDie()
     return r1 + r2 + r3
 
-def playGame(player1, player2):
-    if player1[1] >= 1000 or player2[1] >= 1000:
+def playGame(player0, player1, turn):
+    if player0[1] >= 1000 or player1[1] >= 1000:
         return 0
     else:
+        move = rollThree()
+        if not turn:
+            player0[0] = (player0[0] + move) % 10
+            player0[1] += move
+        else:
+            player1[0] = (player1[0] + move) % 10
+            player1[1] += move
+        return playGame(player0, player1, not turn) + 1
+
 if __name__ == '__main__':
-    player1, player2 = parseInput()
-    print(pos1, pos2)
+    player0, player1 = parseInput()
